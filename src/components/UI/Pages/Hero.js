d114d194f59902950f7b150a776207e8495a868d
jacekhacking <Jacekhacking@mac.com>
2022-07-19 11:54:00 -0600

    hero filler img finished

--- src/components/UI/Pages/Hero.js
+++ src/components/UI/Pages/Hero.js
@@ -1,9 +1,11 @@
 
-import {HeroContainer} from "../Styles/Hero.styles";
+import {HeroContainer, HeroImg} from "../Styles/Hero.styles";
 import  {Header1 } from "../Styles/Global.styles";
+import Img from "../img/hero-holder.jpg"
 const Hero = () => {
     return(
         <HeroContainer>
+            <HeroImg src={Img} alt="" />
             <Header1> SC Bodywork!!!</Header1>
         </HeroContainer>
     )
--- src/components/UI/Styles/Hero.styles.js
+++ src/components/UI/Styles/Hero.styles.js
@@ -1,17 +1,15 @@
 import styled from "styled-components";
-import img from "../img/hero-holder.jpg"
 
 
 export const HeroContainer = styled.div`
-  background-image: url(${img});
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: cover;
-  min-height: 500px;
   display: flex;
   align-items: center;
   justify-content: center;
+  position: relative;
   
 `
+export const HeroImg = styled.img`
+width: 100%;
+`
+
 
