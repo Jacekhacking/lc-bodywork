b47de5e3929ffd4e6ef967410b61265018f1ccfc
JacekHacking <Jacekhacking@mac.com>
2022-08-24 14:33:53 -0600

    about responsiveness altered for best practices

--- src/components/UI/Pages/About.js
+++ src/components/UI/Pages/About.js
@@ -3,28 +3,26 @@ import { JsonData } from "../../../data";
 import BioPic from "../img/biopic.JPG";
 
 const About = () => {
-  const AboutWrapper = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    min-height: 90vh;
-  `;
-
   const AboutHeader = styled.h2`
     padding: 2rem;
     font-size: 2rem;
     text-align: center;
   `;
-  const AboutParagraph = styled.div`
+  const AboutWrapper = styled.div`
     font-size: 1.5rem;
-    line-height: 1.2;
+    line-height: 1.4;
     padding: 30px 100px;
     display: flex;
     align-items: center;
     justify-content: center;
+    min-height: 90vh;
+    @media (max-width: 1200px) {
+      font-size: 1.4rem;
+      padding: 10px 30px;
+    }
+
     img {
-      max-width: 500px;
+      width: min(650px, 65%);
       height: auto;
       padding: 1rem;
     }
@@ -35,18 +33,20 @@ const About = () => {
       max-width: 800px;
       text-align: center;
     }
+
+    @media (max-width: 1300px) {
+      flex-direction: column;
+    }
   `;
 
   return (
     <AboutWrapper>
-      <AboutParagraph>
-        <img src={BioPic} alt="Bio" />
+      <img src={BioPic} alt="Bio" />
 
-        <p>
-          <AboutHeader>{JsonData[0].Bio.title}</AboutHeader>
-          {JsonData[0].Bio.content}
-        </p>
-      </AboutParagraph>
+      <p>
+        <AboutHeader>{JsonData[0].Bio.title}</AboutHeader>
+        {JsonData[0].Bio.content}
+      </p>
     </AboutWrapper>
   );
 };
